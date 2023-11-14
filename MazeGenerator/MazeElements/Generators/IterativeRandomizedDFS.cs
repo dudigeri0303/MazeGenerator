3d9a8d4bc179c1772ba5c151586af6ec3d3cc66d
dudigeri0303 <dudigeri@freemail.hu>
2023-11-15 00:45:46 +0100

    Minor changes

--- MazeGenerator/MazeElements/Generators/IterativeRandomizedDFS.cs
+++ MazeGenerator/MazeElements/Generators/IterativeRandomizedDFS.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MazeGenerator
 {
